0eee654727f1a78c766e426be4dc767808222573
Kimuli Siraje <pworld.designer@gmail.com>
2020-03-20 13:40:11 +0300

    propose

---
+++ layouts/would.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="font-sans text-lg">
+    <nuxt />
+  </main>
+</template>
---
+++ pages/would-you.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-black h-screen w-screen items-center flex justify-center relative">
+    <div class="container">
+      <h1 class="text-white text-5xl leading-tight">
+        <span class="text-5xl">😊</span>
+        I'm grateful to God that I met you
+        <span class="text-yellow font-semibold">ki Gloria.</span>
+      </h1>
+      <p class="text-red mt-6 text-xl">
+        Would you be my <strike>Girlfriend</strike> Queen ?
+      </p>
+
+      <div class="mt-16 bottom-0">
+        <a href="https://wa.me/256783211244?text=Yes" target="_blank" class="font-bold text-xl text-white border-2 border-white py-2 px-4">Yes 😋</a>
+        <a href="https://wa.me/256783211244?text=yes" target="_blank" class="font-bold text-xl text-white border-2 border-white py-2 px-4">Yes 🥵</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYou',
+  layout: 'would'
+}
+</script>
